035f48c53a42c383066aea5f1372273240c2ab8d
maxcaplan <maxacaplan@gmail.com>
2023-01-13 13:36:17 -0400

    Added record gql type def

--- api/resolvers.ts
+++ api/resolvers.ts
@@ -2,4 +2,13 @@ export const resolvers = {
   Query: {
     hello: () => `Hello, World!`,
   },
+  
+  Stars: {
+    ZERO: 0,
+    ONE: 1,
+    TWO: 2,
+    THREE: 3,
+    FOUR: 4,
+    FIVE: 5,
+  },
 };
--- api/typeDefs.ts
+++ api/typeDefs.ts
@@ -4,4 +4,54 @@ export const typeDefs = gql`
   type Query {
     hello: String
   }
+
+  type Record {
+    id: ID!
+    name: String!
+    artists: [String!]!
+    label: String
+    format: Format
+    country: String
+    released: Date!
+    genres: [String!]!
+    disks: [Disk!]!
+    credits: [Credit!]
+    condition: Stars
+    rating: Stars
+  }
+
+  type Format {
+    size: Int!
+    speed: Float
+    weight: Int
+  }
+
+  type Disk {
+    sides: [Side!]!
+  }
+
+  type Side {
+    tracks: [Track!]!
+  }
+
+  type Track {
+    name: String!
+    playtime: Float
+  }
+
+  type Credit {
+    name: String!
+    role: String
+  }
+
+  enum Stars {
+    ZERO
+    ONE
+    TWO
+    THREE
+    FOUR
+    FIVE
+  }
+
+  scalar Date
 `;
