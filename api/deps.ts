56e5b22e39f65541d1a7e576bd890d61403a504d
maxcaplan <maxacaplan@gmail.com>
2023-08-26 14:59:29 -0300

    Node + Typescript boilerplate

--- api/deps.ts
+++
@@ -1,8 +0,0 @@
-// Mongo Client library
-export { Bson, MongoClient } from "https://deno.land/x/mongo@v0.31.2/mod.ts";
-// HTTP Server library
-export { Server } from "https://deno.land/std@0.190.0/http/server.ts";
-// GraphQL libraries
-export { GraphQLHTTP } from "https://deno.land/x/gql@1.2.3/mod.ts";
-export { makeExecutableSchema } from "https://esm.sh/@graphql-tools/schema@10.0.0";
-export { gql } from "https://deno.land/x/graphql_tag@0.1.2/mod.ts";
